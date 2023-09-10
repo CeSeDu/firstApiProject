3900cc5c59ef5b95f97d982afc9fb27cc98fd4e5
Sena Durmuş <senad8127@gmail.com>
2023-09-10 14:55:43 +0300

    fixed end
    Signed-off-by: Sena Durmuş <senad8127@gmail.com>

--- script.js
+++ script.js
@@ -3,41 +3,45 @@ async function Api() {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error('API çağrısı başarısız');
+            throw new Error('API çağrısı başarısız: ' + response.statusText);
         }
-        const element = await response.json();
-        country(element);
+        const countries = await response.json();
+        createCards(countries); // Tüm ülke verilerini kullanarak kartları oluşturun
+        console.log(countries)
     } catch (err) {
         console.error("API doğru çalışmadı: " + err.message);
     }
 }
 
-function country(element) {
-    for (let data of element) {
-        const countryName = data.name.common;
-        const countryCapital = data.capital[0];
+function createCards(countries) {
+    const generalcard = document.querySelector(".general__card");
+
+    for (let data of countries) {
+        const countryName = data.name;
+        const countryCapital = data.capital ? data.capital : 'Bilgi yok'; // capital özelliği mevcut değilse 'Bilgi yok' olarak ayarlayın
         const countryPopulation = data.population;
         const countryGini = data.gini || 'Bilgi yok';
-        const countryFlag = data.flags.png;
+        const countryLanguage = data.languages[0].name || 'Bilgi yok';
+        const countryFlag = data.flags.png || 'Bilgi yok'; // flags.png özelliği mevcut değilse 'Bilgi yok' olarak ayarlayın
 
-        creat(countryName, countryCapital, countryPopulation, countryGini, countryFlag);
+        createCard(countryName, countryCapital, countryPopulation, countryGini, countryLanguage, countryFlag, generalcard);
     }
 }
 
-function creat(name, capital, population, gini, flags) {
-    let add = `<div class="card">
+function createCard(name, capital, population, gini, languages, flags, container) {
+    let add = `
     <div class="country__name">${name}</div>
     <div class="country__flag"><img class="flag" src="${flags}" alt=""></div>
     <div class="country__capital"><span>Capital :</span>${capital}</div>
     <div class="country__population"><span>Population :</span>${population}</div>
+    <div class="country__border"><span>Language :</span>${languages}</div>
     <div class="country__gini"><span>Gini :</span>${gini}</div>
-</div>`;
+`;
 
-    const generalcard = document.querySelector(".general__card");
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = add;
-    generalcard.appendChild(card);
+    container.appendChild(card);
 }
 
 Api();
